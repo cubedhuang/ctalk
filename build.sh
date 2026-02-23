6ce94dfd156507a1f27e6bfbac6c0aed7f76ff5c
cubedhuang <99danielh@gmail.com>
2026-02-22 19:32:09 -0500

    prompt does not exist

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-clang -std=c23 -Wall -Wextra -Werror -pedantic -o main main.c client.c server.c utils.c prompt.c -lpthread -lreadline
+clang -std=c23 -Wall -Wextra -Werror -pedantic -o main main.c client.c server.c utils.c -lpthread -lreadline
 
